642aa49d4cdc4a52e0e92511326b5af48fdb2119
JPannn <js768js@gmail.com>
2023-03-19 05:30:58 -0500

    Fixed the errors, working on the next button

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 // Quiz Information | Questions and Answers
 const questionNumber = document.querySelector('[data-question-number]'); // questionNumber tells user which question they are on
-questionNumber.dataset.currentQuestion = 0;
 const questionText = document.querySelector('[data-question-text]'); // questionText displays the question
+const displayAnswer = document.querySelector('[data-display-answer]'); // checkAnswer checks if the answer is correct or not
 // Answer Buttons
 const answerButtons = document.querySelectorAll('[data-answer-button]');
 // Quiz Controls | Start, Next, Results, Restart
@@ -14,73 +14,92 @@ const nextButton = document.querySelector('[data-next-button]');
 const results = document.querySelector('[data-get-results]');
 const restartButton = document.querySelector('[data-restart-button]');
 // Questions and Answers for the Quiz
-
 const quizQuestions = [
-    questions = {
-        question1: "What game did Mario first appear in?", // 1
-        answers1: [
+    // create key-value pairs for each question and answer from the quizQuestions array
+    {
+        question: "What game did Mario first appear in?",
+        answers: [
             {text: "Super Mario Bros.", correct: false},
             {text: "Donkey Kong", correct: true},
             {text: "Game & Watch", correct: false},
             {text: "Metroid", correct: false}
-        ],
-        question: "Nicknamed King Koopa, who is the arch nemesis of Mario?",// 2
+        ]
+    },
+
+    {
+        question: "Nicknamed King Koopa, who is the arch nemesis of Mario?",
         answers: [
             {text: "Bowser", correct: true},
             {text: "Wario", correct: false},
             {text: "Donkey Kong", correct: false},
             {text: "Diddy Kong", correct: false}
-        ],
-        question: "What is the name of the princess that Mario must save?", // 3
+        ]
+    },
+    {
+        question: "What is the name of the princess that Mario must save?",
         answers: [
             {text: "Peach", correct: true},
             {text: "Daisy", correct: false},
             {text: "Rosalina", correct: false},
             {text: "Toadstool", correct: false}
-        ],
-        question: "What is the name of the mushroom that gives Mario super powers?", // 4
+        ]
+    },
+    {
+        question: "What is the name of the mushroom that gives Mario super powers?",
         answers: [
             {text: "Super Mushroom", correct: true},
             {text: "Mega Mushroom", correct: false},
             {text: "Ultra Mushroom", correct: false},
             {text: "Mushroom", correct: false}
-        ],
-        question: "What is the name of the turtle that helps Mario in his adventures?", // 5
+        ]
+    },
+    {
+        question: "What is the name of the turtle that helps Mario in his adventures?",
         answers: [
             {text: "Toad", correct: false},
             {text: "Yoshi", correct: true},
             {text: "Koopa", correct: false},
             {text: "Goomba", correct: false}
-        ],
-        question: "What is the name of the castle that Bowser lives in?", // 6
+        ]
+    },
+    {
+        question: "What is the name of the castle that Bowser lives in?",
         answers: [
             {text: "Bowser's Castle", correct: true},
             {text: "Bowser's Fortress", correct: false},
             {text: "Bowser's Keep", correct: false},
             {text: "Bowser's Tower", correct: false}
-        ],
-        question: "What is the name of the game that Mario and Luigi first appeared in?", // 7
+        ]
+    },
+    {
+        question: "What is the name of the game that Mario and Luigi first appeared in?",
         answers: [
             {text: "Super Mario Bros.", correct: false},
             {text: "Donkey Kong", correct: false},
             {text: "Game & Watch", correct: false},
             {text: "Mario Bros.", correct: true}
-        ],
-        question: "Mario was named after a real estate developer, what was his name?", // 8
+        ]
+    },
+    {
+        question: "Mario was named after a real estate developer, what was his name?",
         answers: [
             {text: "Mario Abdi", correct: false},
             {text: "Mario Mine", correct: false},
             {text: "Anne T.Kwayted", correct: false},
             {text: "Mario Segale", correct: true}
-        ],
-        question: "Which of the following is NOT a profession Mario has accepted?", // 9
+        ]
+    },
+    {
+        question: "Which of the following is NOT a profession Mario has accepted?",
         answers: [
             {text: "Plumber", correct: false},
             {text: "Firefighter", correct: false},
             {text: "Doctor", correct: false},
             {text: "Graphic Designer", correct: true}
-        ],
-        question: "Mario was known as what before the American release of Donkey Kong?", // 10
+        ]
+    },
+    {
+        question: "Mario was known as what before the American release of Donkey Kong?",
         answers: [
             {text: "Jumpman", correct: true},
             {text: "Sonic", correct: false},
@@ -89,30 +108,53 @@ const quizQuestions = [
         ]
     }
 ];
+
 // initialize the quiz
-generateQuiz(quizQuestions);
-
-
+generateQuiz();
 // Start Quiz Function
 
-function generateQuiz(quizQuestions) {
+function generateQuiz() {
+
     startQuiz();
-    // nextButton();
+    nextButton();
 
     function startQuiz() {
         // Display the first question number and question along with answers, once the start button is clicked.
         startButton.addEventListener('click', () => {
             // Hide the start button
             startButton.classList.add('hide');
-            questionNumber.dataset.currentQuestion = 1;
-            questionNumber.innerText = `${questionNumber.dataset.currentQuestion}/10 `;
-            for(const questions in quizQuestions) {
-                questionText.innerText = quizQuestions[questions].question1;
-                answerButtons.forEach((button, index) => {
-                    button.innerText = quizQuestions[questions].answers1[index].text;
-                });
-            };
+            // Display the first question number and question along with answers
+            questionNumber.innerText = 1;
+            questionText.innerText = quizQuestions[0].question;
+            answerButtons.forEach((button, index) => {
+                button.innerText = quizQuestions[0].answers[index].text;
+            });
+            // Check if the answer is correct or not
+            correctIncorrect();
         });
+    }
+
+    function correctIncorrect() {
+        // check answer button for correct or incorrect after questionAnswers is displayed
+        answerButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                // enable the answer button
+                button.disabled = false;
+                if(quizQuestions[0].answers[index].correct !== true) {
+                    console.log("Incorrect");
+                    displayAnswer.innerText = "Incorrect!";
+                } else {
+                    console.log("Correct");
+                    displayAnswer.innerText = "Correct!";
+                    // disable all answer buttons after correct answer is selected
+                    answerButtons.forEach((button) => button.disabled = true);
+                }
+            });
+        });
+    }
+
+    function nextButton() {
+        // Display the next question number and question along with answers, once the next button is clicked.
     }
 
 }
